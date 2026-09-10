04e8c362005b5473999b276f20736077243014cb
Vyni Tchuelachue <mrvynitchuelachue@gmail.com>
2026-09-10 12:50:50 +0100

    Revert phone icon to its default (unmirrored) orientation

--- client/src/components/FloatingCallButton.tsx
+++ client/src/components/FloatingCallButton.tsx
@@ -9,7 +9,7 @@ const FloatingCallButton = () => {
       className="fixed bottom-28 right-5 z-40 flex h-14 w-14 items-center justify-center rounded-full bg-red-gradient text-white shadow-red transition-transform duration-300 hover:scale-110 active:scale-95"
     >
       <span className="absolute inset-0 animate-ping rounded-full bg-red/60" />
-      <Phone size={22} className="relative -scale-x-100 fill-white" />
+      <Phone size={22} className="relative fill-white" />
     </a>
   );
 };
